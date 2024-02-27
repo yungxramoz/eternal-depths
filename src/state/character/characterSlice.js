103caba1895dc2399e340d15bc97f151ba4fd785
Sandro Gerber <sandro.gerber.ext@snf.ch>
2024-02-27 10:25:26 +0100

    Adds local state handling

--- src/state/character/characterSlice.js
+++ src/state/character/characterSlice.js
@@ -1,24 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
-import Character from "../../models/Character";
+import { createSlice } from '@reduxjs/toolkit'
+import Character from '../../models/Character'
 
 const initialState = {
-    current:  new Character(),
-    availableAttributePoints: 2,
-};
+  current: new Character(),
+  availableAttributePoints: 2,
+}
 
 export const characterSlice = createSlice({
-    name: "character",
-    initialState,
-    reducers: {
-        assignAttributePoint: (state, action) => {
-            if (state.availableAttributePoints > 0) {
-                state.current[action.payload]++;
-                state.availableAttributePoints--;
-            }
-        },
+  name: 'character',
+  initialState,
+  reducers: {
+    setName: (state, { payload }) => {
+      state.current.name = payload
     },
-});
+    setLook: (state, { payload }) => {
+      state.current.look = payload
+    },
+    assignAttributePoint: (
+      state,
+      { payload: { strength, health, agility, precision } },
+    ) => {
+      const total = strength + health + agility + precision
+      if (state.availableAttributePoints >= total) {
+        state.current.strength += strength
+        state.current.health += health
+        state.current.agility += agility
+        state.current.precision += precision
+        state.availableAttributePoints -= total
+      }
+    },
+  },
+})
 
-export const { assignAttributePoint } = characterSlice.actions;
+export const { setName, setLook, assignAttributePoint } = characterSlice.actions
 
-export default characterSlice.reducer;
+export default characterSlice.reducer
--- src/views/NewCharacter/NewCharacter.js
+++ src/views/NewCharacter/NewCharacter.js
@@ -1,19 +1,65 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import RpgButton from '../../components/atoms/RpgButton/RpgButton'
+import IconButton from '../../components/molecules/IconButton/IconButton'
 import RpgContainer from '../../components/templates/RpgContainer/RpgContainer'
 import RpgSeparator from '../../components/templates/RpgSeperator/RpgSeparator'
+import {
+  assignAttributePoint,
+  setName,
+} from '../../state/character/characterSlice'
 import './NewCharacter.css'
-import IconButton from '../../components/molecules/IconButton/IconButton'
-import { useDispatch, useSelector } from 'react-redux'
-import { assignAttributePoint } from '../../state/character/characterSlice'
 
 const NewCharacter = () => {
-  const character = useSelector((state) => state.character.current)
-  const availableAttributePoints = useSelector(
-    (state) => state.character.availableAttributePoints,
-  )
-
   const dispatch = useDispatch()
+  const character = useSelector((state) => state.character.current)
+  const [points, setPoints] = useState(
+    structuredClone(
+      useSelector((state) => state.character.availableAttributePoints),
+    ),
+  )
+  const [assignedPoints, setAssignedPoints] = useState({
+    health: 0,
+    strength: 0,
+    agility: 0,
+    precision: 0,
+  })
+  const [localName, setLocalName] = useState(character.name)
+
+  const localStats = useMemo(() => {
+    return {
+      health: character.health + assignedPoints.health,
+      strength: character.strength + assignedPoints.strength,
+      agility: character.agility + assignedPoints.agility,
+      precision: character.precision + assignedPoints.precision,
+    }
+  })
+
+  const updateStats = (stat, value) => {
+    if (value < 0 && assignedPoints[stat] === 0) return
+    if (value > 0 && points === 0) return
+    setAssignedPoints({
+      ...assignedPoints,
+      [stat]: assignedPoints[stat] + value,
+    })
+    setPoints(points - value)
+  }
+
+  const createCharacter = () => {
+    dispatch(setName(localName))
+    dispatch(assignAttributePoint(assignedPoints))
+    resetLocalFields()
+  }
+
+  const resetLocalFields = () => {
+    setLocalName('')
+    setAssignedPoints({
+      strength: 0,
+      health: 0,
+      agility: 0,
+      precision: 0,
+    })
+  }
 
   return (
     <RpgContainer fullPage>
@@ -24,11 +70,16 @@ const NewCharacter = () => {
         <div className="rpgui-icon helmet-slot" />
         <IconButton icon="chevron-right" />
       </div>
-      <input type="text" placeholder="Name" value={character.name} />
+      <input
+        type="text"
+        placeholder="Name"
+        value={localName}
+        onChange={(e) => setLocalName(e.target.value)}
+      />
       <RpgSeparator />
       <RpgContainer golden className="attribute-container">
         <h2>Attributes</h2>
-        <p>Available points: {availableAttributePoints}</p>
+        <p>Available points: {points}</p>
         <RpgSeparator golden={false} />
         <div className="attribute">
           <p>Health</p>
@@ -36,13 +87,13 @@ const NewCharacter = () => {
             <IconButton
               icon="minus"
               size="medium"
-              onClick={() => dispatch(assignAttributePoint('health'))}
+              onClick={() => updateStats('health', -1)}
             />
-            <p>{character.health}</p>
+            <p>{localStats.health}</p>
             <IconButton
               icon="plus"
               size="medium"
-              onClick={() => dispatch(assignAttributePoint('health'))}
+              onClick={() => updateStats('health', 1)}
             />
           </div>
         </div>
@@ -52,13 +103,13 @@ const NewCharacter = () => {
             <IconButton
               icon="minus"
               size="medium"
-              onClick={() => dispatch(assignAttributePoint('strength'))}
+              onClick={() => updateStats('strength', -1)}
             />
-            <p>{character.strength}</p>
+            <p>{localStats.strength}</p>
             <IconButton
               icon="plus"
               size="medium"
-              onClick={() => dispatch(assignAttributePoint('strength'))}
+              onClick={() => updateStats('strength', 1)}
             />
           </div>
         </div>
@@ -68,13 +119,13 @@ const NewCharacter = () => {
             <IconButton
               icon="minus"
               size="medium"
-              onClick={() => dispatch(assignAttributePoint('agility'))}
+              onClick={() => updateStats('agility', -1)}
             />
-            <p>{character.agility}</p>
+            <p>{localStats.agility}</p>
             <IconButton
               icon="plus"
               size="medium"
-              onClick={() => dispatch(assignAttributePoint('agility'))}
+              onClick={() => updateStats('agility', 1)}
             />
           </div>
         </div>
@@ -84,19 +135,19 @@ const NewCharacter = () => {
             <IconButton
               icon="minus"
               size="medium"
-              onClick={() => dispatch(assignAttributePoint('precision'))}
+              onClick={() => updateStats('precision', -1)}
             />
-            <p>{character.precision}</p>
+            <p>{localStats.precision}</p>
             <IconButton
               icon="plus"
               size="medium"
-              onClick={() => dispatch(assignAttributePoint('precision'))}
+              onClick={() => updateStats('precision', 1)}
             />
           </div>
         </div>
       </RpgContainer>
       <div className="btn-container">
-        <RpgButton text="Create" />
+        <RpgButton text="Create" onClick={createCharacter} />
       </div>
     </RpgContainer>
   )
