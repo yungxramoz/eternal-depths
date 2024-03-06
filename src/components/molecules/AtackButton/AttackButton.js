25aaec8a406723341509bc6c46b9736802e00dcc
Sandro Gerber <sandro.gerber.ext@snf.ch>
2024-03-06 10:35:29 +0100

    Adds cooldown animation

--- src/components/molecules/AtackButton/AttackButton.js
+++ src/components/molecules/AtackButton/AttackButton.js
@@ -4,8 +4,19 @@ import './AttackButton.css'
 
 const AttackButton = ({ onClick, attack, disabled }) => {
   const attackImgSrc = require(`../../../assets/images/attacks/${attack.fileName}`)
+
+  const cooldownPercentage =
+    attack.currentCooldown === 0
+      ? 61
+      : 61 - ((attack.currentCooldown / attack.cooldown) * 61)
+  const cooldownStyle = {
+    width: `${cooldownPercentage}px`,
+    visibility: attack.currentCooldown === 0 ? 'hidden' : 'visible',
+  }
+
   return (
     <button className="attack-button" onClick={onClick} disabled={disabled}>
+      <div className="cooldown-container" style={cooldownStyle}></div>
       <RpgContainer golden2 className="attack-container">
         <img className="attack-icon" src={attackImgSrc} alt={attack.name} />
       </RpgContainer>
